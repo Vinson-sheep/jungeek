b033a4ebce1fb6233164fe0174eaaa3ac9e57901
vinson <775014077@qq.com>
2017-10-24 21:06:06 +0800

    singleday 1

--- manage.py
+++ manage.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+import pymysql
+import json
+
+import urllib
+from urllib import parse
+
+# database message
+database = "jnugeek"
+username = "root"
+password = "abc123"
 
 app = Flask(__name__)
 
@@ -32,6 +42,90 @@ def data_show():
 @app.route('/join_us',methods=['GET'])
 def join_us():
 	return render_template('join_us.html')
+
+@app.route('/singleday',methods=['GET'])
+def singleDay():
+	return render_template('singleDay.html')	
+
+# follow two APIs belong a group
+# singleDayApiPost Function is for posting form
+# as well as posting the data into mysql database
+@app.route('/singleday/api',methods=['POST'])	
+def singleDayApiPost():
+	# use request object to get data
+	# a = request.get_data()
+	# dict1 = json.loads(a)
+	# get data from dict1
+	# gender = dict1['gender']
+	# question = dict1['question']
+	# answer = dict1 ['answer']
+	# options = dict1['options']
+	# wechat = dict1['wechat']
+	gender = urllib.parse.unquote(request.form['gender'])
+	question = urllib.parse.unquote(request.form['question'])
+	answer = urllib.parse.unquote(request.form['answer'])
+	options = urllib.parse.unquote(request.form['options'])
+	wechat = urllib.parse.unquote(request.form['wechat'])
+	# link to database
+	db = pymysql.connect("localhost", username, password, database,use_unicode=True, charset="utf8")
+	# get cursor
+	cursor = db.cursor()
+	# write sql statemens
+	sql = '''insert into singleday (time,gender,question,answer,options,wechat)
+			values (now(), '%s', '%s', '%s', '%s', '%s') ''' % (gender, question, answer, options, wechat)
+	try:
+		cursor.execute(sql)
+		#when you change database, you should commit
+		db.commit()
+	except:
+		db.rollback()
+		return "fail"
 	
+	db.close()
+	return "successful"
+	
+# With singleDayApiGet Function
+# we can get the whole datasingleday of singleday table
+@app.route('/singleday/api',methods=['GET'])
+def singleDayApiGet():
+	# initialize data object
+	data = []
+	# make an dict for value changing
+	gender_change = {
+		"female" : "女",
+		"male" : "男",
+		"both" : "both"
+	}
+	# connect to mysql database
+	db = pymysql.connect('localhost', username, password, database,use_unicode=True, charset="utf8")
+	# get cursor
+	cursor = db.cursor()
+	# write mysql statements
+	sql = '''select time,gender,question,answer,options,wechat from singleday
+			order by time desc'''
+	try:
+		cursor.execute(sql)
+		result = cursor.fetchall()
+		for row in result:
+			b = {}
+			b['time'] = row[0]
+			b['gender'] = gender_change[row[1]]
+			b['question'] = row[2]
+			b['answer'] = row[3]
+			b['options'] = row[4]
+			b['wechat'] = row[5]
+			data.append(b)
+		# after for circle
+		data = json.dumps(data)
+		print (data)
+		
+	except:
+		print('error in singleDayApiGet Function')
+		
+		
+	db.close()
+	return data
+	
+
 if __name__ == '__main__':
     app.run()
---
+++ static/javascript/singleDay.js
@@ -0,0 +1,133 @@
+/**
+ * Created by yys-tt on 2017/10/22.
+ */
+window.onload = function() {
+    // close-btn
+    closeModal();
+    // 初始化提交按钮
+    initSubmit();
+
+};
+
+// 模态框close按钮
+// 点击按钮能够关闭模态框
+function closeModal() {
+    var closeBtn = document.getElementsByClassName("close-btn")[0];
+    var modal = document.getElementsByClassName("modal")[0];
+    closeBtn.onclick = function() {
+        modal.style.display = "none";
+    }
+}
+// 输入参数能够弹出模态框
+// 输入的title和message都为string
+function openModal(title,message) {
+    // 首先验证参数
+    if (!title || typeof title !== typeof ("a") ) {
+        return;
+    }
+    if (!message || typeof message !== typeof ("a") ) {
+        return;
+    }
+    var modal = document.getElementsByClassName("modal")[0];
+    var titleSpan = modal.getElementsByClassName("modal-title")[0];
+    var messagePar = modal.getElementsByTagName("P")[0];
+    titleSpan.innerHTML = title;
+    messagePar.innerHTML = message;
+    modal.style.display = "block";
+}
+
+
+// 提交按钮时间
+// AJAX模拟表单提交
+// 后台是flask框架
+function initSubmit() {
+    // 获取提交按钮
+    var subBtn = document.getElementById("submit");
+    // 初始化点击事件
+    subBtn.onclick = function(e) {
+        // 消除默认行为
+        e.preventDefault();
+        // 禁用按钮
+        subBtn.disabled = true;
+        // 获取表单内容
+        var inputs = document.getElementsByTagName("input");
+        var gender,question,answer,options,wechat;
+        var requireds = [];
+        // 遍历inputs
+        for (var i=0;i<inputs.length;i++) {
+            if (inputs[i].name == "gender" && inputs[i].checked) {
+                // gender
+                gender = inputs[i].value;
+            } else if (inputs[i].name == "question") {
+                // question
+                question = inputs[i].value.trim();
+            } else if (inputs[i].name == "answer") {
+                // answer
+                answer = inputs[i].value.trim();
+            } else if (inputs[i].name == "options") {
+                // options
+                options = inputs[i].value.trim();
+            } else if (inputs[i].name == "wechat"){
+                // wechat
+                wechat = inputs[i].value.trim();
+            }
+            // 获取含有require属性的控件
+            if (inputs[i].required) {
+                requireds.push(inputs[i]);
+            }
+        } /* end for */
+        // 表单验证
+        for (var i=0;i<requireds.length;i++) {
+            if (!requireds[i].value) {
+                // 获取问题内容
+                var title = requireds[i].parentNode.firstElementChild.innerText;
+                // 弹出模态框
+                openModal("提交失败",title+"<br/>为必填");
+                // 解锁按钮
+                subBtn.disabled = false;
+                // 终止事件
+                return false;
+            }
+        }
+        // 过滤
+        var lists = [question,answer,options,wechat];
+        for (var i=0;i<lists.length;i++) {
+            lists[i] = lists[i].replace(/[{}【】|(select)]/g,"");
+        }
+        // 数据处理
+        question = encodeURIComponent(question);
+        answer = encodeURIComponent(answer);
+        options = encodeURIComponent(options);
+        wechat = encodeURIComponent(wechat);
+        var data = "gender=" + gender + "&" + "question=" + question + "&" + "answer=" + answer + "&" + "options=" + options + "&" + "wechat=" + wechat;
+        // AJAX模拟表单提交
+        // 获取地址
+        var url = document.getElementById("form").action;
+        // 新建xhr对象
+        var xhr = new XMLHttpRequest();
+        // xhr操作
+        xhr.open("post", url, false); //同步提交为false，异步可以用true
+        xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");//需要修改header
+        xhr.send(data);//传输数据
+        // 检验提交情况
+        if ((xhr.status >= 200 && xhr.status <300) || xhr.status == 304) {
+            if (xhr.responseText && xhr.responseText == "successful") {
+                // 提交成功
+                openModal("提交成功","上天之doge，祝福着你<br/><br/>页面将于三秒后刷新");
+                setTimeout(function(){location.reload()},3000);
+            } else if (xhr.responseText && xhr.responseText == "fail"){
+                // 提交失败，但是有返回
+                openModal("database出错了","请稍后再试");
+            } else {
+                // 提交失败，可能有返回，但是非successful和fail
+                openModal("发生未知错误","请稍后再试");
+            }
+        } else {
+            // 提交失败
+            // 直接是赤字，大多是网络错误
+            openModal("提交失败","请检查你的网络");
+        }
+        //  解锁按钮
+        subBtn.disabled = false;
+    };
+}
